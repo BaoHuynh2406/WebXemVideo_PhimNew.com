0a031b7957d297e94a04dda1e6a460f05e52484d
PhamQuangGiaBao <150901868+PhamQuangGiaBao@users.noreply.github.com>
2024-04-05 04:34:52 +0700

    Trang tìm kiếm

---
+++ PhimNew.com/src/main/java/Servlet/TimKiem.java
@@ -0,0 +1,66 @@
+package Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import DAO.DAO_Video;
+import Entity.Video;
+
+
+@WebServlet("/timkiem")
+public class TimKiem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TimKiem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8"); // Thiết lập chữ tiếng việt
+		response.setCharacterEncoding("UTF-8"); // Thiết lập chữ tiếng việt
+		
+		
+		
+		try {
+			String NDtimkiem = request.getParameter("nd");
+			System.out.println(NDtimkiem);
+		    DAO_Video daovideo = new DAO_Video();
+		    List<Video> videos = new ArrayList();
+		    videos = daovideo.findByTitle(NDtimkiem);
+		    System.out.println(videos.get(0).getTitle());
+		    
+		    response.setContentType("application/json");
+			PrintWriter out = response.getWriter();
+			out.println(new Gson().toJson(videos));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
